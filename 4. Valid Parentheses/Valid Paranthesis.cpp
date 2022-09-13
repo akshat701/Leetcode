398a1e8f703cd5ad76d3d2fa3f447830c6474046
akshat701 <akshatgoyal2002n@gmail.com>
2022-09-13 19:32:43 +0530

    Valid Paranthesis solution

---
+++ 4. Valid Parentheses/Valid Paranthesis.cpp
@@ -0,0 +1,33 @@
+/* Que Link
+https://leetcode.com/problems/valid-parentheses/
+*/
+
+class Solution {
+public:
+    bool isValid(string s) {stack<char> mystack;
+        for(int i=0;i<s.length();i++){
+            if(mystack.empty()){
+                mystack.push(s[i]);
+                
+            }
+            else if ((mystack.top() == '(' && s[i] == ')')
+                 || (mystack.top() == '{' && s[i] == '}')
+                 || (mystack.top() == '[' && s[i] == ']')){
+                mystack.pop();
+            }
+            else{
+                mystack.push(s[i]);
+            }
+            
+        }
+        if(mystack.empty()){
+            return true;
+            
+        }
+        else{
+            return false;
+        }
+    
+        
+    }
+};
