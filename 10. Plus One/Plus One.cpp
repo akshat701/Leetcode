e09a3b2405b530536ffafc2b944302414f56c261
akshat701 <akshatgoyal2002n@gmail.com>
2022-09-23 21:24:07 +0530

    Plus One solution

---
+++ 10. Plus One/Plus One.cpp
@@ -0,0 +1,21 @@
+/* Que Link: 
+https://leetcode.com/problems/plus-one/
+*/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int count=0;
+       for(int i=digits.size()-1; i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;count++;
+            }
+            else{
+                digits[i]+=1;
+                break;
+            }
+        }
+        if(count==digits.size()){digits.insert(digits.begin(),1);}
+        return digits;
+    }
+};
