afa8fbcb2742f3b7596b3c2fc3a52d9e6a5aa253
akshat701 <akshatgoyal2002n@gmail.com>
2022-09-24 20:31:47 +0530

    Sqrt(x) solution

---
+++ 11.Sqrt(x)/Sqrt(x).cpp
@@ -0,0 +1,14 @@
+/* Que Link:
+https://leetcode.com/problems/sqrtx/
+*/
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        long int n = 0;
+   while(n*n <= x){
+   n++;
+}
+    return n-1;
+    }
+};
