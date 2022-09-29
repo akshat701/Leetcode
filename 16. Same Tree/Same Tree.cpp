359fed7c574129a1e429b589380deb6e14cdcd5c
akshat701 <akshatgoyal2002n@gmail.com>
2022-09-29 23:51:30 +0530

    Same Tree solution

---
+++ 16. Same Tree/Same Tree.cpp
@@ -0,0 +1,20 @@
+/* Que Link: 
+https://leetcode.com/problems/same-tree/
+*/
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+         if(p == NULL &&  q== NULL)
+             return true;
+        
+        if(p == NULL ||  q== NULL)
+            return false;
+        
+        if(p->val != q->val)
+            return false;
+        
+        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left) ;
+    }
+};
