8d71e1c130f11d37d15d1a57915a263b2db6c9c6
akshat701 <akshatgoyal2002n@gmail.com>
2022-10-10 21:16:36 +0530

    Best Time to Buy and Sell Stock solution

---
+++ 26. Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,16 @@
+/*code link:
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+            int min_pr = INT_MAX;
+            for(int i = 0 ;i<prices.size(); i++){
+                    int cost = prices[i] - min_pr;
+                    min_pr = min(min_pr,prices[i]);
+                    profit = max(profit ,cost);
+            }
+            return profit;
+    }
+};
