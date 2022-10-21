763123e087f2972906d4c88587f5b410084a8db0
akshat701 <akshatgoyal2002n@gmail.com>
2022-10-21 21:14:55 +0530

    Majority Element solution

---
+++ 36. Majority Element/Majority Element.cpp
@@ -0,0 +1,16 @@
+/* code link:
+https://leetcode.com/problems/majority-element/
+*/
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> counter;
+        for (int num : nums) {
+            if (++counter[num] > nums.size() / 2) {
+                return num;
+            }
+        }
+        return 0;
+    }
+};
